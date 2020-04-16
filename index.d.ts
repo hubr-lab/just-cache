25f8fad3f25d49c060678f4c9d6fe461b9436441
Gleisson Mattos <gleisson.matos@cubos.io>
2020-04-16 19:17:20 -0300

    chore: use declare on typescript

--- index.d.ts
+++ index.d.ts
@@ -1,22 +1,25 @@
-export interface JustCacheOptions {
-    ttl?: number;
-    limit?: number;
-}
 
-export class JustCache {
-    constructor (options?: JustCacheOptions);
-    put(key: string, value: any): void;
-    put(key: string, value: any, ttl: number): void;
-    set(key: string, value: any): void;
-    set(key: string, value: any, ttl: number): void;
-    clean(): void;
-    has(key: string): boolean;
-    get<T = any>(key: string): T | null;
-    delete(key: string): void;
-    keys(): string[];
-    count(): number;
-    size(): number;
-    sizeText(): string;
-}
 
-export default JustCache;
+declare module "just-cache" {
+    export interface JustCacheOptions {
+        ttl?: number;
+        limit?: number;
+    }
+
+    export default class JustCache {
+        constructor (options?: JustCacheOptions);
+        put(key: string, value: any): void;
+        put(key: string, value: any, ttl: number): void;
+        set(key: string, value: any): void;
+        set(key: string, value: any, ttl: number): void;
+        clean(): void;
+        has(key: string): boolean;
+        get<T = any>(key: string): T | null;
+        delete(key: string): void;
+        keys(): string[];
+        count(): number;
+        size(): number;
+        sizeText(): string;
+    }
+ }
+
