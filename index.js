f881fd703a1a81fe1c12fd5e98161d09cf744071
Gleisson Mattos <gleisson.matos@cubos.io>
2020-04-17 01:57:12 -0300

    refactor: separe cache size manager responsability

--- index.js
+++ index.js
@@ -47,6 +47,84 @@ function isGlobalSymbol(object) {
 class MemoryManager {
 
   /**
+   * Boolean bytes size
+   * @returns {number}
+   */
+  static get BOOLEAN_SIZE() {
+    return 4;
+  }
+
+  /**
+   * Number bytes size
+   * @returns {number}
+   */
+  static get NUMBER_SIZE() {
+    return 8;
+  }
+
+  /**
+   * Size unities
+   * @returns {string[]}
+   */
+  static get SIZE_UNITY() {
+    return ["bytes", "KiB", "MiB", "GiB", "TiB"];
+  }
+
+  /**
+   * Get Array size
+   * @param {Array} element
+   */
+  static getArraySize(element) {
+    let byte = 0;
+    for (const value of element) {
+      byte += this.getSize(value);
+    }
+    return byte;
+  }
+
+  /**
+   * Get Object lenght
+   * @param {object|Array} element
+   */
+  static getObjectSize(element) {
+    if (Array.isArray(element)) {
+      return this.getArraySize(element);
+    }
+
+    let byte = 0;
+    for (const key in element) {
+      byte += this.getSize(element[key]);
+    }
+    return byte;
+  }
+
+  /**
+   * Get Symbol bytes size
+   * @param {Symbol} element
+   */
+  static getSymbolSize(element) {
+    return isGlobalSymbol(element)
+      ? Symbol.keyFor(element).length * 2
+      : (element.toString().length - 8) * 2;
+  }
+
+  /**
+   * Get string bytes size
+   * @param {string} element
+   */
+  static getStringSize(element) {
+    return element.length * 2;
+  }
+
+  /**
+   * Get Buffer bytes size
+   * @param {Buffer} element
+   */
+  static getBufferSize(element) {
+    return element.length;
+  }
+
+  /**
 	 * Get element byte size
 	 * @param {*} element
 	 * @returns {number}
@@ -54,35 +132,22 @@ class MemoryManager {
   static getSize(element) {
 
     if (Buffer.isBuffer(element)) {
-      return element.length;
+      return this.getBufferSize(element);
     }
 
     switch (typeof element) {
         case "object":
-          if (Array.isArray(element)) {
-            let byte = 0;
-            for (const value of element) {
-              byte += this.getSize(value);
-            }
-            return byte;
-          } else {
-            let byte = 0;
-            for (const key in element) {
-              byte += this.getSize(element[key]);
-            }
-            return byte;
-          }
+          return this.getObjectSize(element);
         case "symbol":
-          return isGlobalSymbol(element)
-            ? Symbol.keyFor(element).length * 2
-            : (element.toString().length - 8) * 2;
+          return this.getSymbolSize(element);
         case "string":
-          return element.length * 2;
+          return this.getStringSize(element);
         case "boolean":
-          return 4;
+          return this.BOOLEAN_SIZE;
         case "number":
-          return 8;
+          return this.NUMBER_SIZE;
         default:
+          // TODO: check others formats
           return 0;
     }
   }
@@ -97,22 +162,22 @@ class MemoryManager {
       throw new Error("value size it's not a number");
     }
 
-    const sizes = ["bytes", "KiB", "MiB", "GiB", "TiB"];
-
     if (bytes === 0) {
-      return `0 ${sizes[0]}`;
+      return `0 ${this.SIZE_UNITY[0]}`;
     }
 
+    // Get size unity level
     const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10);
 
     if (i === 0) {
-      return `${bytes} ${sizes[i]}`;
+      return `${bytes} ${this.SIZE_UNITY[i]}`;
     }
-    if (!sizes[i]) {
+
+    if (!this.SIZE_UNITY[i]) {
       return `${bytes} bytes`;
     }
 
-    return `${(bytes / (1024 ** i)).toFixed(1)} ${sizes[i]}`;
+    return `${(bytes / (1024 ** i)).toFixed(1)} ${this.SIZE_UNITY[i]}`;
   }
 }
 
--- test/cache.size.test.js
+++ test/cache.size.test.js
@@ -19,6 +19,34 @@ describe("Just Cache size", () => {
     expect(cache.sizeText()).toBe("14 bytes");
   });
 
+  test("Should returns valid only Buffer size", () => {
+    const cache = new JustCache();
+
+    cache.put("key", new Buffer("this is one buffer"));
+
+    expect(cache.size()).toBe(18);
+    expect(cache.sizeText()).toBe("18 bytes");
+  });
+
+  test("Should returns valid only boolean size", () => {
+    const cache = new JustCache();
+
+    cache.put("key", true); // 4 bytes
+    cache.put("key2", false); // 4 bytes
+
+    expect(cache.size()).toBe(8);
+    expect(cache.sizeText()).toBe("8 bytes");
+  });
+
+  test("Should returns valid only Symbol size", () => {
+    const cache = new JustCache();
+
+    cache.put("key", Symbol("foo")); // 4 bytes
+
+    expect(cache.size()).toBe(6);
+    expect(cache.sizeText()).toBe("6 bytes");
+  });
+
   test("Should returns 0 to empty cache", () => {
     const cache = new JustCache();
     expect(cache.size()).toBe(0);
